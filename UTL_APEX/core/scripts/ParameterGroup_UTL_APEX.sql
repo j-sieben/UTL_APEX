a06f0f944f96817a642f2e3a880b57a8c35abd97
Jürgen Sieben <j.sieben@condes.de>
2020-12-15 09:56:53 +0100

    reflect API changes in PIT

--- UTL_APEX/core/scripts/ParameterGroup_UTL_APEX.sql
+++ UTL_APEX/core/scripts/ParameterGroup_UTL_APEX.sql
@@ -9,13 +9,20 @@ begin
   param_admin.edit_parameter(
     p_par_id => 'ITEM_PREFIX_CONVENTION'
    ,p_par_pgr_id => 'UTL_APEX'
-   ,p_par_description => 'Sets the convention used to prefix page items.'   ,p_par_integer_value => 1   ,p_par_boolean_value => null   ,p_par_is_modifiable => null   ,p_par_validation_string => q'^#NUMBER_VAL# in (1, 2, 3)^'   ,p_par_validation_message => q'^Only one of the utl_apex.CONVENTION_... values are allowed.^'
+   ,p_par_description => 'Sets the convention used to prefix page items.'
+   ,p_par_integer_value => 1
+   ,p_par_boolean_value => null
+   ,p_par_is_modifiable => null
+   ,p_par_validation_string => q'^#INTEGER# in (1, 2, 3)^'
+   ,p_par_validation_message => q'^Only one of the utl_apex.CONVENTION_... values are allowed.^'
   );
 
   param_admin.edit_parameter(
     p_par_id => 'ITEM_VALUE_CONVENTION'
    ,p_par_pgr_id => 'UTL_APEX'
-   ,p_par_description => 'Sets the convention on whether reading a session value from a non existing item throws an error or not.'   ,p_par_boolean_value => null   ,p_par_is_modifiable => null
+   ,p_par_description => 'Sets the convention on whether reading a session value from a non existing item throws an error or not.'
+   ,p_par_boolean_value => null
+   ,p_par_is_modifiable => null
   );
 
   commit;
