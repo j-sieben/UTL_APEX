c7ca52bf77f4a4568c7c4fb512fc7d16ef1c68e9
Jürgen Sieben <j.sieben@condes.de>
2022-10-12 13:35:53 +0200

    Install script stabilized

--- UTL_APEX/install_scripts/init.sql
+++ UTL_APEX/install_scripts/init.sql
@@ -25,8 +25,8 @@ select lower(data_type) || '(' ||
          case when data_type in ('CHAR', 'VARCHAR2') then data_length || case char_used when 'B' then ' byte)' else ' char)' end
          else data_precision || ', ' || data_scale || ')'
        end FLAG_TYPE,
-       case when data_type in ('CHAR', 'VARCHAR2') then dbms_assert.enquote_literal(pit_util.c_true) else pit_util.c_true end C_TRUE, 
-       case when data_type in ('CHAR', 'VARCHAR2') then dbms_assert.enquote_literal(pit_util.c_false) else pit_util.c_false end C_FALSE
+       case when data_type in ('CHAR', 'VARCHAR2') then dbms_assert.enquote_literal(pit_util.c_true) else to_char(pit_util.c_true) end C_TRUE, 
+       case when data_type in ('CHAR', 'VARCHAR2') then dbms_assert.enquote_literal(pit_util.c_false) else to_char(pit_util.c_false) end C_FALSE
   from all_tab_columns
  where table_name = 'PARAMETER_LOCAL'
    and column_name = 'PAL_BOOLEAN_VALUE';
